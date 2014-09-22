dbf3283e4f5500072dd7139521b6fd7fd165857f
Augustine Dunn <wadunn83@gmail.com>
2014-09-22 11:52:57 -0400

    user/views.py: - added render_template_or_404(url): to wrap render_template - changed def members() to point to users/members - changed def checkout() to point to users/checkout

--- TsetseCheckout/user/views.py
+++ TsetseCheckout/user/views.py
@@ -1,11 +1,30 @@
 # -*- coding: utf-8 -*-
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, abort
 from flask.ext.login import login_required
+from jinja2 import TemplateNotFound
 
 blueprint = Blueprint("user", __name__, url_prefix='/users', static_folder="../static")
 
 
-@blueprint.route("/")
+def render_template_or_404(url):
+    """
+    Wraps flask.render_template in a try: except: abort(404)
+    :param url:
+    :return:
+    """
+    try:
+        return render_template(url)
+    except TemplateNotFound:
+        abort(404)
+
+
+@blueprint.route("/members")
 @login_required
 def members():
-    return render_template("users/members.html")
+    return render_template_or_404("users/members.html")
+
+
+@blueprint.route("/checkout")
+@login_required
+def checkout():
+    return render_template_or_404("users/checkout.html")
